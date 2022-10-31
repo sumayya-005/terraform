f2b9d961a7f71a756ea5ac6f27a4ebf1f6378725
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2022-10-31 17:14:57 +0530

    file

--- variables/var.tf
+++ variables/var.tf
@@ -9,3 +9,17 @@ output "sample" {
 output "sample2"{
   value ="Hey,${var.sample}"
 }
+
+#string data type
+variable "sample1"{
+  default = "Hello world"
+}
+
+#number data type
+variable "sample3"{
+  default = 1700
+}
+
+variable "sample4" {
+  default = false
+}
