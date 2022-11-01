efeffdbf98844cbce5860926d70a05d9f0447c29
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2022-11-01 23:05:22 +0530

    file

--- resources/01/ec2.tf
+++ resources/01/ec2.tf
@@ -1,12 +1,21 @@
 provider "aws" {
+
   region ="us-east-1"
+
 }
 
 resource"aws_instance" "sample" {
-  ami = "ami-00d48a21603b2119b"
+
+  ami           = "ami-00d48a21603b2119b"
   instance_type = "t3.micro"
+
+  root_block_device {
+    volume_size = 15
+  }
 }
 
 output "ec2_public_ip" {
+
   value = aws_instance.sample.public_ip
+
 }
